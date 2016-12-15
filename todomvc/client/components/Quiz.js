7cbb994a33a73eb2a422fe9e2aec3d2cf259f999
vibhash1083 <vibhash1083.07@bitmesra.ac.in>
2016-12-15 15:13:07 +0530

    updated urls

--- todomvc/client/components/Quiz.js
+++ todomvc/client/components/Quiz.js
@@ -2,22 +2,31 @@ import React, { PropTypes, Component } from 'react';
 
 import {connect} from 'react-redux';
 
+import {Link} from 'react-router';
+
+
 export default class QuesSection extends Component {
 
 
   render() {
     const { ques, actions } = this.props;
     console.log('in ListSection',this.props);
-    if (this.props.ques);
-            {
-                var questions = this.props.ques.map(function(ques,i){return <li> {ques.question}</li>})
-            }
+
+    if (this.props.ques)
+    {
+                var output = this.props.ques.map(function(ques,i){return <li> {ques.question}</li>}) || 'Nothing'
+    }
 
     return (
               <div className='ListSection'>
+              <h3>Questions</h3>
                 <ul>
-                    {questions}
+                    {output}
                 </ul>
+
+                <Link to={"quiz/i+1"}>Next Question</Link>
+                <br/><br/>
+                <Link to={"/"}>Admin Mode</Link>
               </div>
             );
     }
