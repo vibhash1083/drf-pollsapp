d07fd421cbf7ff2c33a95e14c5129ba16f267e06
vibhash1083 <vibhash1083.07@bitmesra.ac.in>
2016-12-16 19:08:52 +0530

    updated stores

--- todomvc/client/containers/Root.js
+++ todomvc/client/containers/Root.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import DevTools from './DevTools';
 import QuesApp from './QuesApp';
 
 import { Router, browserHistory } from 'react-router';
--- todomvc/client/store/configureStore.prod.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import promise from 'redux-promise';
-import rootReducer from '../reducers';
-
-export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState, applyMiddleware(promise));
-}
