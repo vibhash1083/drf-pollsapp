31db1392d8acb72c885d186dc01f22286de43dfc
eswar <eswarmavuru1607@gmail.com>
2016-12-21 18:23:00 +0530

    Added Choices.

---
+++ quizapp/client/components/Choices.js
@@ -0,0 +1,12 @@
+import React,{ PropTypes, Component } from 'react';
+
+
+export default class Choices extends Component {
+    render() {
+        return(
+                <div>
+                    Choices
+                </div>
+            );
+    }
+}
