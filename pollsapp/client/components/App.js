091e7d29c59b4953073c00861415a3dbcc8e4a28
vibhash1083 <vibhash1083.07@bitmesra.ac.in>
2016-12-23 19:21:43 +0530

    Updated single and deleted extra files

--- pollsapp/client/components/App.js
+++ pollsapp/client/components/App.js
@@ -32,6 +32,7 @@ class App extends Component {
     return (
           <label>
             {ques_div}
+            
           </label>
     );
   }
--- pollsapp/client/components/Single.js
+++ pollsapp/client/components/Single.js
@@ -10,12 +10,12 @@ class Single extends Component {
 
     constructor(props){
         super(props);
-        this.state = {selectedOption:0};
+        this.state = {selectedOption:0, formsubmit: 0};
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(event){
-        this.setState({selectedOption: event.target.value});    
+        this.setState({selectedOption: event.target.value, formsubmit: 0});    
     }
 
     handleSubmit(event) {
@@ -33,6 +33,7 @@ class Single extends Component {
             })
 
         this.getUpdate();
+        this.setState({selectedOption: 0, formsubmit: 1});
     }
 
     getUpdate(){
@@ -40,41 +41,64 @@ class Single extends Component {
     }
 
     render() {
-        console.log('this.state',this.state);
+        console.log('State ', this.state);
         const { questions,choices, actions } = this.props;
-        console.log('in Single',this.props,'question',questions,'choices',choices);
+        console.log('In Single',this.props,'question',questions,'choices',choices);
         const {id} = this.props.params;
         console.log('Index', id);
         console.log('Props', this.props);
         console.log('End Props');
 
-        const i = questions.findIndex(x => x.id==id);
+        const i = questions.findIndex(x => x.id == id);
         const question_obj = questions[i];
-        console.log('question_obj',question_obj);
-        console.log('question_obj id',question_obj.id);
-        console.log('Choice 3rd obj',this.props.choices[1].question);
+        console.log('Question Obj', question_obj);
+        console.log('Question Obj Id', question_obj.id);
 
         const choices_list = this.props.choices;
         console.log('choices_list',choices_list);
         const question_choice = choices.filter((option) => option.question == question_obj.id);
         console.log('question_choice',question_choice);
 
-        return (
-                  <div className='ListSection'>
-                      <h2>Poll Your Choice</h2>
-                        <ul><p>{question_obj.id}. {question_obj.question_text}</p></ul>
-                        <form onSubmit={this.handleSubmit.bind(this)}>
-                        {question_choice.map((option,i) =>
-                            <ul><label>
-                            <input type='radio' key={i} name='{option.id}'
-                            value={option.id} onChange={this.handleChange.bind(this)}/>
-                            {option.choice_text  } Votes : {option.votes}<br/>
-                            </label></ul>
-                            )}
+        let single_content = null;
 
-                        <input type='submit' value='Submit'/>
-                        </form>
-                  </div>
+        if(this.state.formsubmit == 0)
+        {
+            single_content = <div className='ListSection'>
+                                  <h2>Poll Your Choice</h2>
+                                    <ul><p>{question_obj.id}. {question_obj.question_text}</p></ul>
+                                    <form onSubmit={this.handleSubmit.bind(this)}>
+                                    {question_choice.map((option,i) =>
+                                        <ul><label>
+                                        <input type='radio' key={i} name='{option.id}'
+                                        value={option.id} onChange={this.handleChange.bind(this)}/>
+                                        {option.choice_text  } <br/>
+                                        </label></ul>
+                                        )}
+
+                                    <input type='submit' value='Submit'/>
+                                    </form>
+                            </div>
+        }
+        else
+        {
+            single_content = <div className='ListSection'>
+                                  <h2>Votes Summary</h2>
+                                    <ul><p>{question_obj.id}. {question_obj.question_text}</p></ul>
+                                    {question_choice.map((option,i) =>
+                                        <ul><label>
+                                        {option.choice_text  } Votes : {option.votes}<br/>
+                                        </label></ul>
+                                        )}
+                                    <h3>Thank You</h3>
+                                <Link to={"/"}>Go Back to the list of questions</Link>
+
+                            </div>
+        }
+
+        return (
+                <div>
+                  {single_content}
+                </div>
                 );
     }
 }
